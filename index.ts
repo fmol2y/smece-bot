8bb858eacbb6f41b6233addf07632e2a15a9cfe4
Filip <moldvaifilip@gmail.com>
2024-11-14 16:30:24 +0100

    add console logs

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("Starting bot...");
+
 const TOKEN: string = process.env.TELEGRAM_BOT_TOKEN!;
 const CHAT_ID: string | number = process.env.TELEGRAM_CHAT_ID!;
 
@@ -13,6 +15,7 @@ const messageText = 'Danas je dan za smece!!!';
 
 const sendMessage = async () => {
   try {
+    console.log("Sending message...");
     const response = await bot.sendMessage(CHAT_ID, messageText, { message_thread_id: Number.parseInt(process.env.TELEGRAM_MESSAGE_THREAD_ID!) });
     console.log('Message sent', response);
     return;
@@ -24,6 +27,7 @@ const sendMessage = async () => {
 // Scheduling a message for Monday and Thursday at noon CET (Central European Time)
 // "0 12 * * 1,4" means noon (12:00) on Mondays (1) and Thursdays (4)
 cron.schedule('0 12 * * 1,4', () => {
+  console.log("Scheduling message...");
   sendMessage().then(() => {
     console.log("Scheduled message sent");
   }).catch((error) => {
